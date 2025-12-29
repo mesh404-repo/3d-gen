95490ed3f6302bec941c6ae216279fa3be3b87fd
star <star@gmail.com>
2025-12-29 09:16:06 +0900

    change prompt image

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ import time
 
 url = "http://localhost:10006/generate"
 start_time = time.time()
-response = requests.post(url, files={"prompt_image_file": open("cr7.png", "rb")})
+response = requests.post(url, files={"prompt_image_file": open("bo.png", "rb")})
 end_time = time.time()
 print(f"Time taken: {end_time - start_time} seconds")
 
